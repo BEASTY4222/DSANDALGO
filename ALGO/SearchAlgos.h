9e5ccc2900b553e75234a7842a067d6b4480d4fc
Ivan Georgiev <ivangeorgiev181@gmail.com>
2026-08-14 20:01:46 +0300

    been a long day enought for today more after one week of vacation

--- ALGO/SearchAlgos.h
+++ ALGO/SearchAlgos.h
@@ -1,4 +1,33 @@
+#pragma once
+
+#include "DS/BasicDS.h"
 
 namespace MySearchAlgo{
+    //Requirments:
+    //  sorted input
+    //  constant-time random access
+    // O(Log(n))
+    template <class T> // https://roadmap.sh/ai/course/mastering-binary-search-algorithms-and-applications 
+    int binarySearch(const MyDses::DArray<int>& arr, const int& target){
+        if(arr.empty) return -1;
+
+        size_t left = arr[0];
+        size_t right = arr.last();
+        
+        size_t middle = std::tolower((right / 2) - left);
+        for(size_t i{};i < arr.size();++i){
+            
+        }
+
+        return -1;
+    }
+
+    template <class T> // O(n)
+    T linearSearch(const MyDses::DArray<T>& arr, const T& target){
+        if(arr.empty()) return -1;
+
+        for(size_t i{};i < arr.size();++i) if(arr[i] == target) return i;
+        return -1;
+    }
     
 }
