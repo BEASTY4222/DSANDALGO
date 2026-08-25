cd02cf8f0852fe4c2960b3ce1bfd760716d6f462
Ivan Georgiev <ivangeorgiev181@gmail.com>
2026-08-25 15:46:38 +0300

    implemented insertion sort

--- ALGO/SortingAlgos.h
+++ ALGO/SortingAlgos.h
@@ -39,6 +39,20 @@ namespace MySortingAlgo{
         }
     }
 
+    MyDses::DArray<int> insertionSort(MyDses::DArray<int>& arr){
+        for(int i = 1; i < arr.size(); i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+        return arr;
+    }
+
     void selectionSort(){
 
     }
